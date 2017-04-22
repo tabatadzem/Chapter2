eb0d96236ae77774500132af40ba45a87421358e
tabatadzem <tabatadzem@hotmail.com>
2017-04-22 16:30:29 +0200

    new exercise

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -1,15 +1,22 @@
-/**
- * Created by Maestro on 22.04.2017.
- */
+
 import java.util.Scanner;
 
 public class Chapter2 {
     public static void main (String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("please enter degree in Fahrenheit ");
-        double fahrenheit = input.nextDouble();
-        double celsius = (5.0 / 9) * (fahrenheit - 32);
-        System.out.println(fahrenheit + "is " + celsius + "degree");
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("please enter degree in Fahrenheit ");
+//        double fahrenheit = input.nextDouble();
+//        double celsius = (5.0 / 9) * (fahrenheit - 32);
+//        System.out.println(fahrenheit + "is " + celsius + "degree");
+
+        long totalMilliseconds = System.currentTimeMillis(); // Obtain the total milliseconds since midnight, Jan 1, 1970
+        long totalSeconds = totalMilliseconds / 1000; // Obtain the total seconds since midnight, Jan 1, 1970
+        long currentSecond = totalSeconds % 60; // Compute the current second in the minute in the hour
+        long totalMinutes = totalSeconds / 60; // Obtain the total minutes
+        long currentMinutes = totalMinutes % 60; // compute the current minute
+        long totalHours = totalMinutes/60; // obtain the total hours
+        long currentHour = totalHours%24; // compute the current hour
+        System.out.println("currrent time is" + currentHour + ":" + currentMinutes + ":" + currentSecond);
 
 
 
