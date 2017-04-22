38724f00151552db2a222bb5c613ba1ef674d3df
tabatadzem <tabatadzem@hotmail.com>
2017-04-22 16:09:24 +0200

    exercise added

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -1,8 +1,15 @@
 /**
  * Created by Maestro on 22.04.2017.
  */
+import java.util.Scanner;
+
 public class Chapter2 {
     public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter degree in Fahrenheit ");
+        double fahrenheit = input.nextDouble();
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        System.out.println(fahrenheit + "is " + celsius + "degree");
 
 
 
