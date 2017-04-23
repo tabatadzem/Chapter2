9c411a672d3b92cc413280ecfc4cfe29aebf48a1
tabatadzem <tabatadzem@hotmail.com>
2017-04-23 14:54:38 +0200

    exercies 4

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -10,19 +10,44 @@ public class Chapter2 {
 //        System.out.println(fahrenheit + "is " + celsius + "degree");
 
 //
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter the total amount: ");
+//        double totalLoan = input.nextDouble();
+//        System.out.print("Enter the annual interest rate: ");
+//        double annualInterestRate = input.nextDouble();
+//        double monthlyInterestRate = annualInterestRate / 1200;
+//        System.out.print("Enter numbers of the years: ");
+//        int year = input.nextInt();
+//        double monthlyPayment = totalLoan *  monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, year * 12));
+//        double totalPayment = monthlyPayment * year * 12;
+//        System.out.println("Your monthly payment is " + ((int)monthlyPayment * 100) /100.0);
+//        System.out.print("Your total payment is " + (int)totalPayment * 100 / 100.0);
+//        int b = 3, a=1, c = 1;
+//        System.out.println(Math.pow((Math.pow(b,2) - 4* a * c),0.5));
+
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the total amount: ");
-        double totalLoan = input.nextDouble();
-        System.out.print("Enter the annual interest rate: ");
-        double annualInterestRate = input.nextDouble();
-        double monthlyInterestRate = annualInterestRate / 1200;
-        System.out.print("Enter numbers of the years: ");
-        int year = input.nextInt();
-        double monthlyPayment = totalLoan *  monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, year * 12));
-        double totalPayment = monthlyPayment * year * 12;
-        System.out.println("Your monthly payment is " + ((int)monthlyPayment * 100) /100.0);
-        System.out.print("Your total payment is " + (int)totalPayment * 100 / 100.0);
+        System.out.println("Please enter the amount ");
+        double amount = input.nextDouble();
+        int remainingAmount = (int)(amount * 100);
+        int numberOfOneDollars = remainingAmount / 100;
+        remainingAmount = remainingAmount % 100;
+        int numberOfQuarters = remainingAmount / 25;
+        remainingAmount = remainingAmount % 25;
+        int numberOfDimes = remainingAmount / 10;
+        remainingAmount = remainingAmount % 10;
+        int numberOfNickles = remainingAmount / 5;
+        remainingAmount = remainingAmount % 5;
+        int numberOfPennies = remainingAmount;
+
+        System.out.println(" your amount is ");
+        System.out.println(" " + numberOfOneDollars + " dollars" );
+        System.out.println(" " + numberOfQuarters + " Quarters" );
+        System.out.println(" " + numberOfDimes + " Dimes" );
+        System.out.println(" " + numberOfNickles + " Nickles" );
+        System.out.println(" " + numberOfPennies + " pennies");
+
 
 
     }
