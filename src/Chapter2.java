8dc94e982afbda9848f74861598ef77cfadcde1e
tabatadzem <tabatadzem@hotmail.com>
2017-04-23 13:30:11 +0200

    exercies 3

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -9,17 +9,20 @@ public class Chapter2 {
 //        double celsius = (5.0 / 9) * (fahrenheit - 32);
 //        System.out.println(fahrenheit + "is " + celsius + "degree");
 
-        long totalMilliseconds = System.currentTimeMillis(); // Obtain the total milliseconds since midnight, Jan 1, 1970
-        long totalSeconds = totalMilliseconds / 1000; // Obtain the total seconds since midnight, Jan 1, 1970
-        long currentSecond = totalSeconds % 60; // Compute the current second in the minute in the hour
-        long totalMinutes = totalSeconds / 60; // Obtain the total minutes
-        long currentMinutes = totalMinutes % 60; // compute the current minute
-        long totalHours = totalMinutes/60; // obtain the total hours
-        long currentHour = totalHours%24; // compute the current hour
-        System.out.println("currrent time is" + currentHour + ":" + currentMinutes + ":" + currentSecond);
+//
 
-
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the total amount: ");
+        double totalLoan = input.nextDouble();
+        System.out.print("Enter the annual interest rate: ");
+        double annualInterestRate = input.nextDouble();
+        double monthlyInterestRate = annualInterestRate / 1200;
+        System.out.print("Enter numbers of the years: ");
+        int year = input.nextInt();
+        double monthlyPayment = totalLoan *  monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, year * 12));
+        double totalPayment = monthlyPayment * year * 12;
+        System.out.println("Your monthly payment is " + ((int)monthlyPayment * 100) /100.0);
+        System.out.print("Your total payment is " + (int)totalPayment * 100 / 100.0);
 
 
     }
